66e7cc4898d164093008e7c5113b1fa74c3afc54
Caroline <caroline@MacBook-Air-de-Caroline.local>
2017-02-19 13:58:26 +0100

    liste des featured playlists

--- Spoty/Spoty/HomeViewController.m
+++ Spoty/Spoty/HomeViewController.m
@@ -11,7 +11,10 @@
 
 @interface HomeViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 @end
+
 
 @implementation HomeViewController
 
@@ -19,8 +22,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"HOME VIEW CONTROLLER");
-    //[self.coService featurePlaylist];
+    // on passe par le AppDeleguate pour accéder au service de connexion pour éviter les circular references
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     [appDelegate.coService featurePlaylist:self];
     
@@ -29,6 +31,9 @@
 -(void)receivePlaylists:(NSMutableDictionary*)data {
     self.playlists = data;
     NSLog(@"HomeView has now the data : %@", self.playlists);
+    [self.tableView setDataSource:self];
+    [self.tableView reloadData];
+    NSLog(@"On a les titres : %@", [self.playlists objectForKey:@"name"]);
 }
 
 
@@ -40,5 +45,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"Notre liste doit faire cette taille : %lu", (unsigned long)[[self.playlists objectForKey:@"name"] count]);
+    return [[self.playlists objectForKey:@"name"] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"unePlaylist";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    // get the names
+    NSString *playName = [[self.playlists objectForKey:@"name"] objectAtIndex:indexPath.row];
+    NSString *playNumber = [[[self.playlists objectForKey:@"tracks"] objectAtIndex:indexPath.row] objectForKey:@"total"];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ : %@ morceaux", playName, playNumber];
+    //[cell.textLabel.text stringByAppendingString:@" : %@ tracks", [[self.playlists objectForKey:@"tracks"] objectForKey:@"total"]];
+    //NSLog(@"On fait la ligne : %d", indexPath.row);
+    
+    return cell;
+}
 
 @end
